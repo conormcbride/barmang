a22868f3a1dd9e3e668fddfa507981952de03421
Conor McBride <conormcbride158@gmail.com>
2017-12-11 22:38:54 +0000

    Lists working

--- public/javascripts/controllers/barlistcontroller.js
+++ public/javascripts/controllers/barlistcontroller.js
@@ -1,7 +1,20 @@
 var app = angular.module('BarManager');
 
-app.controller('barlistController', ['$scope', function($scope) {
+app.controller('barlistController', ['$scope', '$http', function($scope, $http) {
     // create a message to display in our view
     $scope.message = 'Bar List!';
+
+    findAll();
+
+    function findAll() {
+        $http.get('/bar')
+            .success(function (data) {
+                $scope.bars = data;
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
 }
 ]);
--- public/javascripts/controllers/stafflistcontroller.js
+++ public/javascripts/controllers/stafflistcontroller.js
@@ -1,7 +1,20 @@
 var app = angular.module('BarManager');
 
-app.controller('stafflistController', ['$scope', function($scope) {
+app.controller('stafflistController', ['$scope', '$http', function($scope, $http) {
     // create a message to display in our view
     $scope.message = 'Staff List!';
+
+    findAll();
+
+    function findAll() {
+        $http.get('/staff')
+            .success(function (data) {
+                $scope.staffs = data;
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
 }
 ]);
