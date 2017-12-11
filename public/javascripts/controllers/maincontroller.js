5f04bb82671fea77eac6e4ec01167800a8dca2cd
Conor McBride <conormcbride158@gmail.com>
2017-12-11 20:33:17 +0000

    Fixed home page

---
+++ public/javascripts/controllers/maincontroller.js
@@ -0,0 +1,8 @@
+var app = angular.module('BarManager');
+
+
+app.controller('mainController', ['$scope', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Bar manager!!';
+}
+]);
