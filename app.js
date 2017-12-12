d94454816577e2c1cc2a22edf1ca8907c85f4462
Conor McBride <conormcbride158@gmail.com>
2017-12-12 19:53:49 +0000

    complete build script set

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var prod =  process.env.NODE_ENV === 'prod';
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,7 +22,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));     // CHANGED
+if (prod) {
+    app.use(express.static(path.join(__dirname, 'dist')));
+} else {
+    app.use(express.static(path.join(__dirname, 'build')));
+}     // CHANGED
 app.use("/public", express.static(__dirname + "/public"));
 
 app.use('/', index);
