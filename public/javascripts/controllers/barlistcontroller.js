9dcb8994bd23b89ac113b6cc33562aad03626893
Conor McBride <conormcbride158@gmail.com>
2017-12-12 01:15:35 +0000

    delete and edit working

--- public/javascripts/controllers/barlistcontroller.js
+++ public/javascripts/controllers/barlistcontroller.js
@@ -16,5 +16,37 @@ app.controller('barlistController', ['$scope', '$http', function($scope, $http) 
                 console.log('Error: ' + data);
             });
     };
-}
-]);
+
+    $scope.delete = function (id) {
+        if (confirm("are you sure you want to delete this ?")) {
+            console.log('Deleting id :' + id);
+
+            $http.delete('/bar/' + id)
+                .success(function (data) {
+                    console.log(data);
+                    findAll();
+                })
+                .error(function (data) {
+                    console.log('Error: ' + data);
+                })
+        }
+    }
+    $scope.current = {};
+
+    $scope.update = function (bar) {
+        console.log(bar._id);
+        $scope.current = bar;
+    };
+
+    $scope.save = function () {
+        console.log($scope.current._id);
+        $http.put('bar/' + $scope.current._id + '/update', $scope.current).success(function (data) {
+
+            console.log(data);
+            findAll()
+            $scope.current = ""
+        }).error(function (data) {
+            console.log('Error: ' + data);
+        });
+    }
+}]);
--- public/javascripts/controllers/stafflistcontroller.js
+++ public/javascripts/controllers/stafflistcontroller.js
@@ -16,5 +16,37 @@ app.controller('stafflistController', ['$scope', '$http', function($scope, $http
                 console.log('Error: ' + data);
             });
     };
-}
-]);
+    $scope.delete = function (id) {
+        if (confirm("are you sure you want to delete this ?")) {
+            console.log('Deleting id :' + id);
+
+            $http.delete('/staff/' + id)
+                .success(function (data) {
+                    console.log(data);
+                    findAll();
+                })
+                .error(function (data) {
+                    console.log('Error: ' + data);
+                })
+        }
+    }
+
+    $scope.current = {};
+
+    $scope.update = function (staff) {
+        console.log(staff._id);
+        $scope.current = staff;
+    };
+
+    $scope.save = function () {
+        console.log($scope.current._id);
+        $http.put('staff/' + $scope.current._id + '/update', $scope.current).success(function (data) {
+
+            console.log(data);
+            findAll()
+            $scope.current = ""
+        }).error(function (data) {
+            console.log('Error: ' + data);
+        });
+    }
+}]);
